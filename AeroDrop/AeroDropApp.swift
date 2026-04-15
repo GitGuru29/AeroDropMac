2faa78787aab40d5303f94b6420e1e2fd4a1447a
GitGuru29 <sdangalla66@gmail.com>
2026-04-15 11:32:17 +0530

    Initial Commit

---
+++ AeroDrop/AeroDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  AeroDropApp.swift
+//  AeroDrop
+//
+//  Created by siluna on 2026-04-15.
+//
+
+import SwiftUI
+
+@main
+struct AeroDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
